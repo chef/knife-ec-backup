54752a1e9edc6df50caf2e66de3be59a3f9b3163
Jeremy J. Miller <jm@chef.io>
2018-05-16 15:02:36 -0400

    better ui.confirm message
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- lib/chef/knife/ec_base.rb
+++ lib/chef/knife/ec_base.rb
@@ -234,8 +234,8 @@ class Chef
           existing_group_data = FFI_Yajl::Parser.parse(::File.read(clients_group_path), symbolize_names: false)
           existing_group_data['clients'] = [] unless existing_group_data.key?('clients')
           if existing_group_data['clients'].length != clients_in_org.length
-            ui.warn "There are #{(existing_group_data['clients'].length - clients_in_org.length).abs} missing clients in #{org}'s client group file #{clients_group_path}. If this is not intentional do not perform a restore until corrected. `knife tidy backup clean` will auto-correct this. https://github.com/chef-customers/knife-tidy"
-            ui.confirm("\nDo you wish to continue?") if op == "restore"
+            ui.warn "There are #{(existing_group_data['clients'].length - clients_in_org.length).abs} missing clients in #{org}'s client group file #{clients_group_path}. If this is not intentional do NOT proceed with a restore until corrected. `knife tidy backup clean` will auto-correct this. https://github.com/chef-customers/knife-tidy"
+            ui.confirm("\nDo you still wish to continue with the restore?") if op == "restore"
           end
         end
       end
