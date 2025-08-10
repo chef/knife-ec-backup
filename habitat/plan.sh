4afd22f34b5f54055f09d17069da5f0ea46fa0e1
nikhil2611 <nikhilgupta2102@gmail.com>
2025-08-10 12:18:31 +0530

    trying to refrator install function
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -42,13 +42,10 @@ do_install() {
   pushd "${HAB_CACHE_SRC_PATH}/${pkg_dirname}" || exit 1
   cp -R . "$pkg_prefix/"
 
-  echo "Removing unwanted directories"
-  rm -rf "$pkg_prefix/spec" "$pkg_prefix/test" "$pkg_prefix/.bundle" "$pkg_prefix/results"
+  # rm -rf "$pkg_prefix/spec" "$pkg_prefix/test" "$pkg_prefix/.bundle" "$pkg_prefix/results"
 
-  echo "Deleting broken symlinks"
   find "$pkg_prefix" -type l -exec test ! -e {} \; -delete
 
-  echo "Fixing permissions"
   if [ -d "$pkg_prefix/bin" ]; then
     chmod +x "$pkg_prefix/bin"/*
   fi
