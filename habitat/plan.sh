877eb66613bb736630a152648514784a5c0d9b3a
nikhil2611 <nikhilgupta2102@gmail.com>
2025-08-10 20:11:56 +0530

    updated unpack func
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -29,7 +29,8 @@ pkg_deps=(
 
 do_unpack() {
   mkdir -p "${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
-  rsync -a --exclude='.*' $PLAN_CONTEXT/../ ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/
+  # rsync -a --exclude='.*' $PLAN_CONTEXT/../ ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/
+  rsync -a --exclude='.*' --exclude='results/' "$PLAN_CONTEXT/../" "${HAB_CACHE_SRC_PATH}/${pkg_dirname}/"
 }
 
 do_build() {
@@ -44,7 +45,7 @@ do_install() {
 
   # rm -rf "$pkg_prefix/spec" "$pkg_prefix/test" "$pkg_prefix/.bundle" "$pkg_prefix/results"
 
-  rm -rf "$pkg_prefix/results"
+  # rm -rf "$pkg_prefix/results"
 
   # Remove broken symlinks in the source before copying
   find "$pkg_prefix" -type l -exec test ! -e {} \; -delete
